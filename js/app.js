3976eec73325a56333d7263bfa63c37117360c99
sshilal1 <sshilal1@binghamton.edu>
2016-02-19 13:33:11 -0500

    big changes to APP N ENGINE

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    this.xRange = [-150, 600];
+    this.possibleY = [60, 140, 220];
+    this.speedRange = [150, 600];
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.reset();
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    var maxPos = this.xRange[1];
+    this.x += this.speed * dt;
+
+    if (this.x > maxPos) {
+        this.reset();
+    }
+};
+
+Enemy.prototype.reset = function() {
+    var startPos = this.xRange[0];
+
+    this.x = startPos;
+    this.y = this.possibleY[Math.floor(Math.random() * this.possibleY.length)];
+    this.speed = this.getRandomSpeed();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +31,80 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.getRandomSpeed = function() {
+    var minSpeed = this.speedRange[0],
+        maxSpeed = this.speedRange[1];
+    return Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var player = function() {
-
-	this.sprite = 'images/charboy.png';
+// Player initialization
+var Player = function() {
+    this.xRange = [-2, 402];
+    this.yRange = [-20, 380];
+    this.sprite = 'images/char-boy.png';
+    this.playerScore = 0;
+    this.reset();
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Does the real work of the game
+Player.prototype.update = function() {
+    if (this.y == -20) {
+        // player is on water, reset, score one
+        this.scorePoint();
+        this.reset();
+    } else if (this.y >= 60 && this.y <= 220) {
+        var self = this;
+        // player is on road rows, check collisions
+        // loop through each bug
+        allEnemies.forEach(function(enemy) {
+            // is the bug on the same row as the player?
+            if (enemy.y == self.y) {
+                // is the bug on the player?
+                if (enemy.x >= player.x - 30 && enemy.x <= player.x + 30) {                   
+                    self.reset();
+                    self.losePoint();
+                }
+            }
+        });
+    }
+};
 
+// Resets the player to the starting point of the map
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 380;
+};
 
+// Scores a point
+Player.prototype.scorePoint = function() {
+    this.playerScore += 1;
+};
+
+// Subtracts a point
+Player.prototype.losePoint = function() {
+    this.playerScore -= 1;
+};
+
+// Render the location
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Key input handles
+Player.prototype.handleInput = function(key) {
+    if (key === 'left') {
+        this.x -= (this.x - 101 < this.xRange[0]) ? 0 : 101;
+    } else if (key === 'right') {
+        this.x += (this.x + 101 > this.xRange[1]) ? 0 : 101;
+    } else if (key === 'up') {
+        this.y -= (this.y - 80 < this.yRange[0]) ? 0 : 80;
+    } else if (key === 'down') {
+        this.y += (this.y + 80 > this.yRange[1]) ? 0 : 80;
+    }
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -47,3 +118,10 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var allEnemies = [enemy1, enemy2, enemy3];
+
+var player = new Player();
